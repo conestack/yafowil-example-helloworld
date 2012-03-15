edb17c4b731576808615ca1acfc22e98d8bc7590
Jens W. Klein <jens@bluedynamics.com>
2012-03-15 11:49:31 +0100

    minimal

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='helloworld',
       packages=find_packages('src'),
       package_dir = {'': 'src'},
-      install_requires=['setuptools', 'yafowil.webob',]
+      install_requires=['setuptools', 'yafowil.webob',],
       entry_points = """\
       [console_scripts]      
       helloworld = helloworld.run:run    
